8900329c1f55794bb64741cf9f8d25932903729f
KKTRKKT <paulhana6006@gmail.com>
2023-06-08 17:59:19 +0900

    jdbc 사용 예제 추가

--- src/main/java/me/kktrkkt/Application.java
+++ src/main/java/me/kktrkkt/Application.java
@@ -1,8 +1,29 @@
 package me.kktrkkt;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Application {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:postgresql://localhost:15432/postgres";
+        String username = "kktrkkt";
+        String password = "pass";
+
+        try(Connection connection = DriverManager.getConnection(url, username, password)){
+            System.out.println(connection);
+            final String ddl = "CREATE TABLE ACCOUNT (id int, username VARCHAR(255), password VARCHAR(255));";
+            excuteSql(connection, ddl);
+            final String dml = "INSERT INTO ACCOUNT VALUES(1, 'kktrkkt', 'pass');";
+            excuteSql(connection, dml);
+        }
+    }
+
+    private static void excuteSql(Connection connection, String ddl) throws SQLException {
+        try (final PreparedStatement preparedStatement = connection.prepareStatement(ddl)) {
+            preparedStatement.execute();
+        }
     }
 }
